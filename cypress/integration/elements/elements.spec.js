4b5e0ddf7ecd55b02c6468e46e2d060cd1d116dc
Pedro Garcia <pgarciafernandez@gmail.com>
2022-04-06 13:48:51 +0200

    test de elementos

--- cypress/integration/elements/elements.spec.js
+++ cypress/integration/elements/elements.spec.js
@@ -2,7 +2,9 @@
 
 it('check if there are 5 buttons at the bottom of the page', ()=>{
 
-    cy.visit('https://the-internet.herokuapp.com/shifting_content/menu')
+    cy.visit('https://the-internet.herokuapp.com')
+    cy.get(':nth-child(39) > a').click()
+    cy.get('[href="/shifting_content/menu"]').click()
     cy.get('ul li').should('have.length', 5)
     
 } )
